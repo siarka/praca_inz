b70b42794b81687d70f2c9c7dd34c46b173b3c90
siarka <58476699+siarka@users.noreply.github.com>
2023-10-25 22:05:11 +0200

    zapis

--- kamera.py
+++ kamera.py
@@ -1,5 +1,7 @@
 import cv2
-
+# Ustaw parametry zapisu wideo
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Wybierz kodek kompresji (tutaj używamy XVID)
+#out = cv2.VideoWriter('zapisany_film.mp4', fourcc, 20.0, (640, 480))  # Ustaw nazwę pliku, kodek, liczbę klatek na sekundę (fps) i rozdzielczość
 # Otwórz kamerę (numer kamery 0 oznacza kamerę domyślną)
 cap = cv2.VideoCapture("kod.mp4")
 
@@ -15,9 +17,11 @@ while True:
     if not ret:
         break
     cv2.imshow("Kamera", frame)
-
+ #   out.write(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+#out.release()
 cap.release()
 cv2.destroyAllWindows()
+
+
